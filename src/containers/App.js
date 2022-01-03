dd9801e3b7dc5bf6f3d0e9fb3c2c65afd21a70d7
Jan Rolenc <jrolenc@centrum.cz>
2022-01-03 18:51:23 +0100

    first commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,71 +1,49 @@
-import React, { Component } from "react";//!!!
+import React, { useState, useEffect } from "react";
 import CardList from '../components/CardList';
-// import { robots } from './robots'; toto v dalsi fazi nahrazujeme za https://jsonplaceholder.typicode.com/users, abychom jakoze stahovali z externiho serveru
 import SearchBox from "../components/SearchBox";
 import Scroll from '../components/Scroll';
 
+function App() {
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
+    // const [count, setCount] = useState(0);
 
-class App extends Component {//!!!pokud mam v kodu state, tak uz je to tzv. smart komponenta a musim pouzit class
-    constructor() {
-        super()
-        this.state = {
-            //robots: robots,//toto by tu nemuselo byt, protoze ted pouzivam roboty ze souboru a nemeni se, ale vyhledove budou z databaze a ta se muze menit, jak se bude neco zadavat taky uzivatelem
-            robots: [], //kdyz chceme users fetchovat, tak dame tady prazdne pole misto robots a pouzijeme componentDidMount viz nize
-            searchfield: ""
-        }
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => { setRobots(users) });
+    }, [])//tady musim dat druhy parametr - prazdne pole - protoze useEffect se spousti kazdym renderem 
+    //a tak by se to spoustelo stale dokola; nepomohlo by ani kdybych misto [] dal robots, nebo searchfield
+    //jinymi slovy rika, ze to [] je shortcut pro componentDidMount
+    //druhy parametr by mel specifikovat, na cem bude spusteni useEffektu zaviset - napr. dam tlacitko 
+    //s onClick nekde dolu do DOM a novou state promennou count, setCount a nasledne do useEffect jako druhy
+    //param [count] - toto by zpusobilo, ze fetch probehne vzdy az po stisku tlacitka  
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value);
+
     }
-    //detaily jsou na netu pod Components Lifecycle Methods; tuto metodu pouzijeme pro natahovani dat z externiho serveru
-    componentDidMount() {//toto je jedna z component metod, stejne jako construktor nebo render, takze ani nevolame arrow funkci
-        fetch("https://jsonplaceholder.typicode.com/users")//natahuju z externiho API; cele je to syntax, netreba nad tim dumat; fetch patri k window object
-            .then(response => response.json()) //puvodne byla syntaxe s return a {}, ale jde to i bez
-            .then(users => this.setState({ robots: users }));           //stahujeme usery misto robotu
-    }
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
 
-    onSearchChange = (event) => {// !!! puvodne tu mel onSearchChange(event), ale udajne musime takto, aby bylo jasne, k cemu se vztahuje this; pry je to obvykla chyba 
-        this.setState({ searchfield: event.target.value });//!!! pozor na syntax!!! toto je taky pravidlo - musim nastavit state pomoci setState timto zpusobem
-
-    }
-    //abych nize nedaval tolikrat this.state, tak mohu pomoci destructuring:
-    //const {robots, searchfield} = this.state;
-    render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-        });
-        //!!
-        //if-else nize by se dalo profi zlepsit:
-        //return !robots.length ?             -----> robots.length vraci false, pokud je === 0; tedy s ! vraci true a vraci loading; jinak nacita App
-        //<h1>Loading...</h1>
-        //:                                     
-        //(
-        // <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-        //         <div style={{ flexDirection: "column" }}>
-        //             <h1>RoboFriends</h1>
-        //             <SearchBox searchChange={this.onSearchChange} />
-        //         </div>
-        //         <Scroll>
-        //             <CardList robots={filteredRobots} />
-        //         </Scroll>
-
-        //     </div>
-        // );
-
-        if (this.state.robots.length === 0) {
-            return <h1>Loading...</h1>
-        }
-        else {
-            return (
+    return (
+        !robots.length
+            ?
+            <h1>Loading...</h1>
+            : (
                 <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
                     <div style={{ flexDirection: "column" }}>
                         <h1>RoboFriends</h1>
-                        <SearchBox searchChange={this.onSearchChange} />
+                        {/* <button onCLick={() => setCount(count+1)}></button> */}
+                        <SearchBox searchChange={onSearchChange} />
                     </div>
                     <Scroll>
                         <CardList robots={filteredRobots} />
                     </Scroll>
-
                 </div>
-            );
-        }
-    }
+            )
+    );
 }
+
 export default App;
